1ed021c9811741a6a8e1e7eef3585a637bf95b28
Petr Patek <petrpatekml@gmail.com>
2018-07-13 14:35:15 +0200

    created basic server with auth

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-const cfg = require('../local.config.json');
-require('dotenv').config();
-
-
 const app = require('express')();
 const basicAuth = require('express-basic-auth');
+const dotEnv = require('dotenv');
 
+if (process.env.NODE_ENV === 'development') {
+  dotEnv.config({ path: './development.env' });
+}
 app.use(basicAuth({
-  users: { admin: cfg.password },
+  users: { admin: process.env.PASSWORD },
 }));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(process.env.PORT, () => console.log('Example app listening on port 3000!'));
+const server = app.listen(process.env.PORT, () => console.log(`Server started at port: ${server.address().port}`));
